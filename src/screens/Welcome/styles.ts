3b1656b627c2ed359624f265d259bc3b0c7178fa
Gabriel Azevedo <azevgabriel@gmail.com>
2021-08-11 09:19:39 -0300

    Update styles.ts

--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -4,7 +4,7 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: '4.9%',
+        margin: '5%',
         //paddingTop: Platform.OS === 'android' ? 15 : 0,
     }
 })
