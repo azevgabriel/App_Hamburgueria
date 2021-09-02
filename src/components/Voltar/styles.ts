d134b9bf0eafda229536920f2c3405428069c285
Leonardo Henrique Petreca Costa <leopetrecca@gmail.com>
2021-09-02 02:10:47 -0300

    Separar componentes e criação do logo

---
+++ src/components/Voltar/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    marginTop: 40, //Coloquei uma altura que achei interessante, mas podem mudar depois!
+    justifyContent: 'center'
+  } 
+})
