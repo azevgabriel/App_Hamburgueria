f1eab157c602f2ed75a1a12e46019cc8024bd03f
Gabriel Azevedo <azevgabriel@gmail.com>
2021-09-14 18:42:58 -0300

    feat: create Welcome screen

--- src/screens/Welcome/index.tsx
+++ src/screens/Welcome/index.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import {View} from 'react-native';
+import {
+    View,
+    Text,
+    Image,
+} from 'react-native';
 
 import {styles} from './styles';
+
+import Hamburger from '../../assets/hamburger.png';
 
 import Button from '../../components/Button';
 
 export default function Welcome(){
     return(
         <View style={styles.container}>
-            <Button 
-                title="Próximo"
-                color="#000000"
+            <Text style={styles.welcomeText}>
+                Bem vindo, visitante!
+            </Text>
+            <Image
+                style={styles.image}
+                source={Hamburger}
+            />
+            <Text style={styles.titleOne}>
+                HAMBURGUERIA
+            </Text>
+            <Text style={styles.titleTwo}>
+                BOGIATTO
+            </Text>
+            <Text style={styles.label}>
+                Você é novo por aqui?
+            </Text>
+            <Button
+                title="Cadastrar"
+                color="#293845"
+            />
+            <Text style={[styles.label,{marginTop: "8%"}]}>
+                Já tenho cadastro.
+                </Text>
+            <Button
+                title="Entrar"
+                color="#293845"
             />
         </View>
     )
--- src/screens/Welcome/styles.ts
+++ src/screens/Welcome/styles.ts
@@ -4,10 +4,44 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        margin: '5%',
+        marginHorizontal: '15%',
+        marginVertical: '10%',
         //paddingTop: Platform.OS === 'android' ? 15 : 0,
     },
-    headContainer: {
-        flex: 1,
+    welcomeText: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        letterSpacing: -.2,
+        color: '#293845',
+    },
+    image: {
+        alignSelf: 'center',
+        width: '90%',
+        height: '30%',
+        marginVertical: '15%',
+    },
+    titleOne: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        letterSpacing: -.3,
+        color: '#D85B6E'
+    },
+    titleTwo: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        textAlign: 'right',
+        letterSpacing: -.3,
+        color: '#2B7F70',   
+        marginBottom: '15%',
+    },
+    label: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        textAlign: 'left',
+        letterSpacing: -.3,
+        color: '#293845',
+        marginBottom: '1%',
     }
 })
